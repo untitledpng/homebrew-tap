011eba90a4e8ff2a1d9e8a884fab2af2df84370f
Mike Brouwer <mike@brouwer.xyz>
2025-10-02 00:52:38 +0200

    📦 Updated environment-switcher to v1.1.0

--- Formula/environment-switcher.rb
+++ Formula/environment-switcher.rb
@@ -2,7 +2,7 @@ class EnvironmentSwitcher < Formula
   desc "CLI tool to switch between different environment configurations"
   homepage "https://github.com/untitledpng/environment-switcher"
   url "https://github.com/untitledpng/environment-switcher/archive/v1.1.0.tar.gz"
-  sha256 "ca527bd88cc24c8c111364950029506df06937fc8ca1f183055fb6c17cb05634"
+  sha256 "fb291c01b902d59b7b9a7ca712439703de65f5755b30366657b6a270ee4583cb"
   license "MIT"
 
   depends_on :macos
