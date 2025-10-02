81691bcb0d9096b4f2f2f1ba4e5549903315b96c
Mike Brouwer <mikebrouwer@happyhorizon.com>
2025-10-02 16:43:59 +0200

    📦️ Updated untitledpng/environment-switcher to v1.4.0

--- Formula/environment-switcher.rb
+++ Formula/environment-switcher.rb
@@ -1,16 +1,20 @@
 class EnvironmentSwitcher < Formula
   desc "CLI tool to switch between different environment configurations"
   homepage "https://github.com/untitledpng/environment-switcher"
-  url "https://github.com/untitledpng/environment-switcher/archive/v1.3.0.tar.gz"
-  sha256 "95c1ac12518b4e21faf295272c00098fc20e57a27be123106b77415418da256b"
+  version "1.4.0"
   license "MIT"
 
-  depends_on :macos
-  depends_on xcode: ["12.0", :build]
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/untitledpng/environment-switcher/releases/download/v1.4.0/switch-arm64-apple-macos.tar.gz"
+      sha256 "7034f918d62385df2a593d33a96652900aed0ca987866f976442404fc50a0d0b"
+    else
+      odie "This formula only supports Apple Silicon Macs"
+    end
+  end
 
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release"
-    bin.install ".build/release/switch"
+    bin.install "switch"
   end
 
   test do
