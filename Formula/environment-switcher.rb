0717961c7ed5b7deda10fcbf5175b2622132ea57
Mike Brouwer <44807372+untitledpng@users.noreply.github.com>
2025-10-02 15:04:41 +0200

    📦️ Updated untitledpng/environment-switcher to v1.3.0

--- Formula/environment-switcher.rb
+++ Formula/environment-switcher.rb
@@ -1,8 +1,8 @@
 class EnvironmentSwitcher < Formula
   desc "CLI tool to switch between different environment configurations"
   homepage "https://github.com/untitledpng/environment-switcher"
-  url "https://github.com/untitledpng/environment-switcher/archive/v1.5.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/untitledpng/environment-switcher/archive/v1.3.0.tar.gz"
+  sha256 "95c1ac12518b4e21faf295272c00098fc20e57a27be123106b77415418da256b"
   license "MIT"
 
   depends_on :macos
